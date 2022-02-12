4a42917cd2bd522843a9fb323852660d72bc7de2
StakePool <michelleidson@gmail.com>
2022-02-12 00:08:54 -0300

    Add HIGH and LOW in Notification title

--- alert-events.js
+++ alert-events.js
@@ -8,22 +8,64 @@ const NOTIFY_COLOR_MESSAGE = 12263456;
 
 const api = axios.create()
 
+
+function formatStringValue(string_value_text) {
+    const array_key_values = string_value_text.split(" ");
+    array_key_values.splice(0, 1);
+    array_key_values.splice(array_key_values.length - 1, 1)
+    const array_sanitize = array_key_values.map(value => {
+        const raw_text = value.replaceAll(",", "").replaceAll("{", "").replaceAll("}", "").replaceAll("'", "");
+        const splited_text = raw_text.split("=");
+        const output_text = `"${splited_text[0]}":"${splited_text[1]}"`
+        return output_text
+    })
+    const message_test = "{" + array_sanitize.join() + "}";
+    const parsed_test = JSON.parse(message_test);
+    return parsed_test;
+}
+
+function appendAdditionDescription(value_string, low_condition, high_condition){
+    if (typeof value_string !== 'undefined' && typeof low_condition !== 'undefined' && typeof high_condition !== 'undefined') {
+            
+        const parsed_test = formatStringValue(value_string);
+        console.log(parsed_test)
+        let metric_value = parseFloat(parsed_test.value);
+        console.log(metric_value)
+
+        const high_limit = parseFloat(high_condition);
+        const low_limit = parseFloat(low_condition);
+
+        if (metric_value > high_limit) {
+            console.log("HIGH CONDITION")
+            return "HIGH"
+        }
+        if (metric_value < low_limit) {
+            console.log("LOW CONDITION")
+            return "LOW"
+        }
+        return ""
+    }
+}
+
 async function sendDiscordMessage(message) {
-    const { status, alerts } = message;
+    const { status, alerts, commonAnnotations, commonLabels } = message;
     let embeds = [];
 
     if (typeof alerts !== 'undefined') {
 
+        const { __value_string__ } = commonAnnotations;
+        const { low_condition, high_condition } = commonLabels;
+        let aditional_text = appendAdditionDescription(__value_string__,low_condition,high_condition);
+
         alerts.map(alert => {
             embeds.push({
-                "title": `(${alert.status.toUpperCase()}) ${message.commonLabels.alertname.toUpperCase()}`,
-                "description": `${typeof alert.annotations.summary === 'undefined' ? "" : alert.annotations.summary}`,
+                "title": `(${alert.status.toUpperCase()}) ${message.commonLabels.alertname.toUpperCase()} ${aditional_text}`,
+                "description": `${typeof alert.annotations.summary === 'undefined' ? "" : alert.annotations.summary}\n[Silence Alert](https://telemetry.posbaker.com/alertmanager/#/alerts)`,
                 "url": `https://telemetry.posbaker.com/grafana/d/${message.commonAnnotations.__dashboardUid__}?orgId=1&refresh=5s&viewPanel=${message.commonAnnotations.__panelId__}`,
-                "color": (alert.status == 'firing' ? NOTIFY_COLOR_MESSAGE : 4109717),
+                "color": (alert.status == 'firing' ? NOTIFY_COLOR_MESSAGE : 4109717)
             })
 
         })
-
     }
 
     if (typeof alertname !== 'undefined') {
@@ -51,17 +93,22 @@ async function sendDiscordMessage(message) {
 
 async function sendSlackMessage(message) {
 
-    const { status, alerts } = message;
+    const { status, alerts, commonAnnotations, commonLabels } = message;
     let attachments = [];
 
     if (typeof alerts !== 'undefined') {
 
+        const { __value_string__ } = commonAnnotations;
+        const { low_condition, high_condition } = commonLabels;
+        let aditional_text = appendAdditionDescription(__value_string__,low_condition,high_condition);
+
         alerts.map(alert => {
             attachments.push({
-                "title": `(${alert.status.toUpperCase()}) ${message.commonLabels.alertname.toUpperCase()}`,
+                "title": `(${alert.status.toUpperCase()}) ${message.commonLabels.alertname.toUpperCase()} ${aditional_text}`,
                 "text": `${typeof alert.annotations.summary === 'undefined' ? "" : alert.annotations.summary}`,
                 "title_link": `https://telemetry.posbaker.com/grafana/d/${message.commonAnnotations.__dashboardUid__}?orgId=1&refresh=5s&viewPanel=${message.commonAnnotations.__panelId__}`,
-                "color": (alert.status == 'firing' ? '#FA2C23' : "#2DE64F"),
+                "color": (alert.status == 'firing' ? '#FA2C23' : "#2DE64F")
+
             })
 
         })
@@ -70,7 +117,24 @@ async function sendSlackMessage(message) {
 
     const json = JSON.stringify({
         "channel": SLACK_CHANNEL,
-        "attachments": attachments
+        "attachments": attachments,
+        "blocks": [
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Silence Alert",
+                            "emoji": true
+                        },
+                        "value": "click_me_123",
+                        "url": "https://telemetry.posbaker.com/alertmanager/#/alerts"
+                    }
+                ]
+            }
+        ]
     })
 
     console.log("Send notify change Info to Slack:", json)
@@ -81,7 +145,6 @@ async function sendSlackMessage(message) {
     }).catch(err => {
         console.log(err);
     })
-
 }
 
 module.exports = {
